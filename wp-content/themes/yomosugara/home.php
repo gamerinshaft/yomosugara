2c549fa163c1bf75d17839230358144991fc4981
j.taneichi <j.tane3ma@gmail.com>
2015-05-13 13:19:08 +0900

    update

--- wp-content/themes/yomosugara/home.php
+++ wp-content/themes/yomosugara/home.php
@@ -19,97 +19,65 @@ Template Name: home
     <meta name="description" content="">
     <meta name="author" content="Hakim El Hattab">
 
-     <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/lib/mousewheel.js"></script>
-
+     <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/lib/jquery.horizonScroll.js"></script>
+     <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/lib/touchSwipe.js"></script>
 
   </head>
 
   <body>
-    <div id="contents">
-      <div class="smog smog1"></div>
-      <div class="smog smog2"></div>
-      <div class="section">
-        <div class="table">
-          <div class="table-cell">
-            <h1 class="site__title">夜もすがら</h1>
-            <h2 class="site__sub-title">Continue making it all night.</h2>
-          </div>
+    <div class="smog smog1"></div>
+    <div class="smog smog2"></div>
+    <ul class="site__menu">
+      <li class="menu__list">
+        Works
+      </li>
+      <li class="menu__list">
+        History
+      </li>
+      <li class="menu__list">
+        Gallery
+      </li>
+    </ul>
+    <section data-role="section" class="section">
+      <div class="table">
+        <div class="table-cell">
+          <h1 class="site__title">夜もすがら</h1>
+          <h2 class="site__sub-title">Continue making it all night.</h2>
         </div>
-        <ul class="site__menu">
-          <li class="menu__list">
-            Works
-          </li>
-          <li class="menu__list">
-            History
-          </li>
-          <li class="menu__list">
-            Gallery
-          </li>
-        </ul>
       </div>
-      <div class="section">
-        <div class="table">
-          <div class="table-cell">
-            <h1 class="site__title">夜もすがら</h1>
-            <h2 class="site__sub-title">Continue making it all night.</h2>
-          </div>
+    </section>
+    <section data-role="section" class="section">
+      <div class="table">
+        <div class="table-cell">
+          <h1 class="site__title">春はあけぼの</h1>
+          <h2 class="site__sub-title">Continue making it all night.</h2>
         </div>
-        <ul class="site__menu">
-          <li class="menu__list">
-            Works
-          </li>
-          <li class="menu__list">
-            History
-          </li>
-          <li class="menu__list">
-            Gallery
-          </li>
-        </ul>
       </div>
-      <div class="section">
-        <div class="table">
-          <div class="table-cell">
-            <h1 class="site__title">夜もすがら</h1>
-            <h2 class="site__sub-title">Continue making it all night.</h2>
-          </div>
+    </section>
+    <section data-role="section" class="section">
+      <div class="table">
+        <div class="table-cell">
+          <h1 class="site__title">ほげ</h1>
+          <h2 class="site__sub-title">Continue making it all night.</h2>
         </div>
-        <ul class="site__menu">
-          <li class="menu__list">
-            Works
-          </li>
-          <li class="menu__list">
-            History
-          </li>
-          <li class="menu__list">
-            Gallery
-          </li>
-        </ul>
       </div>
-    </div>
+    </section>
     <script type="text/javascript">
       $(function() {
-          $(window).on("resize load", function(){
-            //コンテンツの横サイズ
-            console.log("hoge");
-            var cont = $('#contents');
-            var child = $('.section')
-            var childW = child.outerWidth(true);
-            child.width($(window).outerWidth(true));
-            var contW = childW * $('.section',cont ).length;
-            cont.css('width', contW);
-            $(".smog").css('width', contW);
-          });
-          //スクロールスピード
-            var speed = 2;
-            //マウスホイールで横移動
-            $('html').mousewheel(function(event, mov) {
-                //ie firefox
-                $(this).scrollLeft($(this).scrollLeft() - mov * speed);
-                //webkit
-                $('body').scrollLeft($('body').scrollLeft() - mov * speed);
-                return false;   //縦スクロール不可
-            });
-         $('a[href^=#]').click(function() {
+        $.fn.horizon.defaults = {
+                    scrollTimeout: null,
+                    scrollEndDelay: 50,
+                    scrollDuration: 1000,
+                    i: 0,
+                    limit: 0,
+                    docWidth: 0,
+                    sections: null,
+                    swipe: true,
+                    fnCallback: function (i) {
+                    }
+                  };
+        $('section').horizon();
+        $('a[href^=#]').click(function() {
         var speed = 400;// ミリ秒
         var href= $(this).attr("href");
         var target = $(href == "#" || href == "" ? 'html' : href);
